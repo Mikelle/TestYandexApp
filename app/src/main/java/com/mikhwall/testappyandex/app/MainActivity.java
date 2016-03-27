bed4c27078e1f2b6da2e729d875e3d52adc68d0b
Mikelle <mikhwall@gmail.com>
2016-03-27 23:21:22 +0300

    Init commit

---
+++ app/src/main/java/com/mikhwall/testappyandex/app/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mikhwall.testappyandex.app;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
