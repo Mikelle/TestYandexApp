9781878ef97a221f7e757ba270cb51fd76f1c0e4
Mikelle <mikhwall@gmail.com>
2016-03-30 23:42:50 +0300

    Added disconnect from urlConnection

--- app/src/main/java/com/mikhwall/testappyandex/app/MainActivity.java
+++ app/src/main/java/com/mikhwall/testappyandex/app/MainActivity.java
@@ -9,6 +9,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -53,6 +54,17 @@ public class MainActivity extends AppCompatActivity {
 
             } catch (Exception e) {
                 e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             return resultJson;
         }
