a57c65a763fa7a6eee55c70327ce1d7e0ecf9651
Mikelle <mikhwall@gmail.com>
2016-04-10 23:43:29 +0300

    Added onClickListener

---
+++ app/src/main/java/com/mikhwall/testappyandex/app/activity/ArtistDetailActivity.java
@@ -0,0 +1,18 @@
+package com.mikhwall.testappyandex.app.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mikhwall.testappyandex.app.model.Artist;
+
+public class ArtistDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedIntsanseState) {
+        super.onCreate(savedIntsanseState);
+
+        Intent intent = getIntent();
+    }
+
+}
--- app/src/main/java/com/mikhwall/testappyandex/app/activity/MainActivity.java
+++ app/src/main/java/com/mikhwall/testappyandex/app/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package com.mikhwall.testappyandex.app.activity;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
 
 import com.mikhwall.testappyandex.app.DividerItemDecoration;
 import com.mikhwall.testappyandex.app.R;
@@ -44,6 +49,18 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(new DividerItemDecoration(this));
         adapter = new ArtistAdapter(MainActivity.this, artistsList);
 
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Artist artist = artistsList.get(position);
+                Toast.makeText(getApplicationContext(), artist.getName() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
         new ParseTask().execute();
     }
 
@@ -115,6 +132,56 @@ public class MainActivity extends AppCompatActivity {
             artist.setCover_small(obj.getJSONObject(JSONobj.TAG_COVER).getString(JSONobj.TAG_COVER_SMALL));
             artist.setCover_big(obj.getJSONObject(JSONobj.TAG_COVER).getString(JSONobj.TAG_COVER_BIG));
             return artist;
+        }
+    }
+
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private MainActivity.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final MainActivity.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildLayoutPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildLayoutPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
         }
     }
 }
