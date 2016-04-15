431782fbc512dce5bd133f3729a5c2e0006103cd
Mikelle <mikhwall@gmail.com>
2016-04-15 23:53:40 +0300

    Almost finished DetailActivity

--- app/src/main/java/com/mikhwall/testappyandex/app/activity/ArtistDetailActivity.java
+++ app/src/main/java/com/mikhwall/testappyandex/app/activity/ArtistDetailActivity.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mikhwall.testappyandex.app.R;
+import com.mikhwall.testappyandex.app.helpers.ArtistHelper;
 import com.mikhwall.testappyandex.app.model.Artist;
 import com.mikhwall.testappyandex.app.model.DataTransition;
 import com.squareup.picasso.Picasso;
@@ -27,15 +28,24 @@ public class ArtistDetailActivity extends AppCompatActivity {
     private void makeGUI(Artist artist) {
         setTitle(artist.getName());
 
-        TextView genres = (TextView) findViewById(R.id.detail_genres);
-        TextView info = (TextView) findViewById(R.id.detail_info);
-        TextView biography = (TextView) findViewById(R.id.detail_biography);
-        ImageView image = (ImageView) findViewById(R.id.big_cover);
+        TextView genresTextView = (TextView) findViewById(R.id.detail_genres);
+        TextView infoTextView = (TextView) findViewById(R.id.detail_info);
+        TextView biographyTextView = (TextView) findViewById(R.id.detail_biography);
+        ImageView imageImageView = (ImageView) findViewById(R.id.big_cover);
+
+        String genres = artist.getGenres();
+        String info = ArtistHelper.buildArtistAlbumsInfo(artist, this) + "  .  "
+                + ArtistHelper.buildArtistTracksInfo(artist, this);
+        String biography = artist.getDescription();
+
+        genresTextView.setText(genres);
+        infoTextView.setText(info);
+        biographyTextView.setText(biography);
 
         Picasso.with(this).load(artist.getCover_big())
                 .error(R.drawable.placeholder)
                 .placeholder(R.drawable.placeholder)
-                .into(image);
+                .into(imageImageView);
 
     }
 }
